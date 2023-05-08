fa52be4f9bc3483311e50a652b69a8630bd57ce1
Mykhailo <misha.vrana@gmail.com>
2023-05-08 14:36:27 +0300

    Initial Commit

---
+++ TouchMe/TouchMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchMeApp.swift
+//  TouchMe
+//
+//  Created by Misha Vrana on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
